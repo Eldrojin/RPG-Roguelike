04706df3e2247958c40d4687002b03013a7f2377
Eldrojin <dystraxus@gmail.com>
2016-09-19 09:02:31 -0500

    Revise Basic Movemenet and Add Attack Animation.
    Movement is messed up. Blendtrees required. Finish adding the rest of
    the attack animations later.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,15 +5,19 @@ public class PlayerMove : MonoBehaviour {
     public float Speed = 1.0f;
     public float Distance = .01f;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         Animator animate = GetComponent<Animator>();
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetMouseButtonDown(0))
+        {
+            Speed -= .05f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             Speed += .05f;
             Distance += .01f;
@@ -27,6 +31,7 @@ public class PlayerMove : MonoBehaviour {
             {
                 GetComponent<Transform>().position = new Vector2(transform.position.x - Speed, transform.position.y);
             }
+
         }
         if (Input.GetKeyUp(KeyCode.A))
         {
@@ -62,11 +67,15 @@ public class PlayerMove : MonoBehaviour {
         {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, Distance, 1 << 8);
             animate.SetBool("WalkingUp", true);
+            if (Input.GetMouseButton(0))
+            {
+                animate.SetBool("Attack", true);
+            }
             if (hit.collider == null)
             {
                 GetComponent<Transform>().position = new Vector2(transform.position.x, transform.position.y + Speed);
             }
-        }
+            }
         if (Input.GetKeyUp(KeyCode.W))
         {
             animate.SetBool("WalkingUp", false);
@@ -77,5 +86,11 @@ public class PlayerMove : MonoBehaviour {
             Distance -= .0f;
             GetComponent<Animator>().speed -= .5f;
         }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Speed += .05f;
+            animate.SetBool("Attack", false);
+        }
     }
 }
+
